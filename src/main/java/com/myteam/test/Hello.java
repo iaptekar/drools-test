fb6f1187d9818b5784034d2328b99d5cd1c56033
igoraptekar <igoraptekar@10.9.14.30>
2018-05-22 15:34:36 +0100

    main class

---
+++ src/main/java/com/myteam/test/Hello.java
@@ -0,0 +1,15 @@
+package com.myteam.test;
+
+public class Hello {
+
+	private String greeting;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+}
